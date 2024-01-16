f8e8089ca378e6d9afd177489a39ac6a2a50f82d
abodh5921 <abodh5921@gmail.com>
2024-01-16 13:42:17 +0530

    Create isAdmin middleware and requireSignIn For Tokken

--- ECOMMERCE-APP/controllers/authController.js
+++ ECOMMERCE-APP/controllers/authController.js
@@ -124,3 +124,8 @@ export const loginController = async (req, res) => {
         });
     }
 };
+//test controller
+export const testController = (req,res)=>{
+    
+    res.send("protected Route")
+}
