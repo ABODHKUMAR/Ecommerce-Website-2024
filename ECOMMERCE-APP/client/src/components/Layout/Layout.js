4bddca26a8fa439e57c795a23011e8f029579082
abodh5921 <abodh5921@gmail.com>
2024-01-28 10:44:13 +0530

    React App Components Pages Routing Works

---
+++ ECOMMERCE-APP/client/src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from "./Footer.js";
+import Header from "./Header.js"
+
+
+const Layout = ({children}) => {
+  return (
+    <div>
+    <Header/>
+    <main style={{minHeight:"80vh"}}>{children}</main>
+    <Footer/>
+    </div>
+  )
+}
+
+export default Layout
