f690112532a35cd51c23e5919858a9206cdb3111
abodh5921 <abodh5921@gmail.com>
2024-02-01 20:23:26 +0530

    Products Api CRUD with image

--- ECOMMERCE-APP/server.js
+++ ECOMMERCE-APP/server.js
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js"
 import catagoryRoutes from './routes/categoryRoutes.js'
+import productRoutes from "./routes/productRoutes.js"
 import cors from 'cors';
 //Configuration env
 dotenv.config();
@@ -12,28 +13,29 @@ dotenv.config();
 //database Config
 connectDB();
 //rest object
-const app= express();
+const app = express();
 
 
 //middlewares
 app.use(express.json()); // Used send JSON Data In Req, Send .
-app.use(morgan('dev'));  // Used Show Api Call in Terminal.
+app.use(morgan('dev')); // Used Show Api Call in Terminal.
 app.use(cors());
 
 
 //Routes
-app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/category",catagoryRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", catagoryRoutes);
+app.use("/api/v1/product", productRoutes);
 //rest api
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("<h1>WelCome to ecommerce app</h1>");
 })
 
 
 
 //PORT
-const PORT=process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 })
